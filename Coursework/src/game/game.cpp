9c2a759aac77729d851ed32b793a373a2e21755b
ecowie202 <35229439+ecowie202@users.noreply.github.com>
2018-03-26 16:39:03 +0100

    Add new streetlight

--- Coursework/src/game/game.cpp
+++ Coursework/src/game/game.cpp
@@ -14,21 +14,24 @@ Game::Game() :
 	m_building2Model(m_shader, "res/models/buildings/building07.obj"), // Create building model 2
 	m_building3Model(m_shader, "res/models/buildings/building03.obj"), // Create building model 3
 	m_streetTransform({}, {-0.5f, 0.5f, 0.5f}, {{270}}),
-	m_lampTransform1({-1.5f, 0, -4.25f}, {1,2,1}, {{90}}),
-	m_lampTransform2({-7.5f, 0, -4.25f}, {1,2,1}, {{90}}),
-	m_lampTransform3({-13.f, 0, -4.25f}, {1,2,1}, {{90}}),
+	m_lampTransform1({4.5f, 0, -4.25f}, {1,2,1}, {{90}}),
+	m_lampTransform2({-1.5f, 0, -4.25f}, {1,2,1}, {{90}}),
+	m_lampTransform3({-7.5f, 0, -4.25f}, {1,2,1}, {{90}}),
+	m_lampTransform4({-13.f, 0, -4.25f}, {1,2,1}, {{90}}),
 	m_houseTransform({3.25f, 0, -10}, vec3(0.05f), {{180}}),
 	m_building1Transform({-3.25f, 0, -11}),
 	m_building2Transform({-13.25f, 0, -13.25f}),
 	m_building3Transform1({22.5f, 0, -9}),
 	m_building3Transform2({22.5f, 0, -27}),
-	m_dirLight({0, -1, 0}),
-	m_pointLight1({-1.5f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
-	m_pointLight2({-7.5f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
-	m_pointLight3({-13.f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
-	m_spotLight1({-1.5f, 5.75f, -4.2f}, {-0/*.35f*/, -1, 0}, cos(radians(30.f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f}),
-	m_spotLight2({-7.5f, 5.75f, -4.2f}, {-0/*.35f*/, -1, 0}, cos(radians(30.f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f}),
-	m_spotLight3({-13.f, 5.75f, -4.2f}, {-0/*.35f*/, -1, 0}, cos(radians(30.f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f})
+	m_moonLight({0, -1, 0}, vec3(0), {0, 0, 0.2f}),
+	m_pointLight1({4.5f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
+	m_pointLight2({-1.5f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
+	m_pointLight3({-7.5f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
+	m_pointLight4({-13.f, 5.75f, -4.2f}, 0.7f, 1.8f, 1, vec3(0), {1, 1, 0.5f}),
+	m_spotLight1({4.5f, 5.75f, -4.2f}, {-0, -1, 0}, cos(radians(27.5f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f}),
+	m_spotLight2({-1.5f, 5.75f, -4.2f}, {-0, -1, 0}, cos(radians(27.5f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f}),
+	m_spotLight3({-7.5f, 5.75f, -4.2f}, {-0, -1, 0}, cos(radians(27.5f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f}),
+	m_spotLight4({-13.f, 5.75f, -4.2f}, {-0, -1, 0}, cos(radians(27.5f)), cos(radians(35.f)), 0.045f, 0.0075f, 1, vec3(0), {1, 1, 0.5f})
 {
 	// Enable vertical synchronisation
 	m_window.verticalSync(true);
@@ -38,7 +41,7 @@ Game::Game() :
 
 	// Set up shader
 	m_shader.bind();
-	m_shader.uniform("ambientLight", vec3(0.25f));
+	m_shader.uniform("ambientLight", {0.25f, 0.25f, 0.35f});
 	m_shader.unbind();
 }
 
@@ -70,9 +73,9 @@ void Game::render()
 	// Clear the back buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	vector<DirectionalLight> directionalLights = { /*m_dirLight*/ };
-	vector<PointLight> pointLights = { m_pointLight1, m_pointLight2, m_pointLight3 };
-	vector<SpotLight> spotLights = { m_spotLight1, m_spotLight2, m_spotLight3 };
+	vector<DirectionalLight> directionalLights = { /*m_moonLight*/ };
+	vector<PointLight> pointLights = { m_pointLight1, m_pointLight2, m_pointLight3, m_pointLight4 };
+	vector<SpotLight> spotLights = { m_spotLight1, m_spotLight2, m_spotLight3, m_spotLight4 };
 
 	// Draw the street model
 	m_streetModel.draw(m_streetTransform.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
@@ -81,6 +84,7 @@ void Game::render()
 	m_lampModel.draw(m_lampTransform1.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
 	m_lampModel.draw(m_lampTransform2.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
 	m_lampModel.draw(m_lampTransform3.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
+	m_lampModel.draw(m_lampTransform4.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
 
 	// Draw the house model
 	m_houseModel.draw(m_houseTransform.model(), m_camera.view(), m_viewport.projection(), directionalLights, pointLights, spotLights);
--- Coursework/src/game/game.hpp
+++ Coursework/src/game/game.hpp
@@ -37,17 +37,20 @@ private:
 	Transform m_lampTransform1;
 	Transform m_lampTransform2;
 	Transform m_lampTransform3;
+	Transform m_lampTransform4;
 	Transform m_houseTransform;
 	Transform m_building1Transform;
 	Transform m_building2Transform;
 	Transform m_building3Transform1;
 	Transform m_building3Transform2;
 
-	DirectionalLight m_dirLight;
+	DirectionalLight m_moonLight;
 	PointLight m_pointLight1;
 	PointLight m_pointLight2;
 	PointLight m_pointLight3;
+	PointLight m_pointLight4;
 	SpotLight m_spotLight1;
 	SpotLight m_spotLight2;
 	SpotLight m_spotLight3;
+	SpotLight m_spotLight4;
 };
